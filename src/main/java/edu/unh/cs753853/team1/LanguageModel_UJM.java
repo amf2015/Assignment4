dec45db0366959359e47f97f97f4578d430b53b4
amf2015 <amf2015@wildcats.unh.edu>
2017-10-17 21:53:35 -0400

    Removed .idea/

---
+++ src/main/java/edu/unh/cs753853/team1/LanguageModel_UJM.java
@@ -0,0 +1,52 @@
+package edu.unh.cs753853.team1;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import co.nstant.in.cbor.CborException;
+import edu.unh.cs.treccartool.Data;
+import edu.unh.cs.treccartool.read_data.DeserializeData;
+import edu.unh.cs.treccartool.read_data.DeserializeData.RuntimeCborException;
+
+private class RunFileString {
+    public String queryId;
+    public String paraId;
+    public int rank;
+    public float score;
+    public String teamName;
+    public String methodName;
+
+    RunFileString()
+    {
+
+    }
+}
+
+public class LanguageModel_UJM {
+
+
+    ArrayList<String>
+
+    LanguageModel_UJM(ArrayList<Data.Page> pagelist)
+    {
+
+    }
+}
--- src/main/java/edu/unh/cs753853/team1/QueryParagraphs.java
+++ src/main/java/edu/unh/cs753853/team1/QueryParagraphs.java
@@ -1,32 +1,25 @@
 package edu.unh.cs753853.team1;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.similarities.BasicStats;
-import org.apache.lucene.search.similarities.SimilarityBase;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import co.nstant.in.cbor.CborException;
 import edu.unh.cs.treccartool.Data;
